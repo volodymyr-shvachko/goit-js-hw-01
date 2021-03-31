8c5cbef328763e801e4240c1f91a9a6f8d464716
volodymyr-shvachko <74620985+volodymyr-shvachko@users.noreply.github.com>
2021-03-31 18:25:49 +0300

    Update task-5.js
    asdasdf

--- js/task-5.js
+++ js/task-5.js
@@ -6,24 +6,24 @@ const country = prompt('Введите название страны')
 switch (country.toLowerCase()) {
   case 'китай':
     cost = 100
-    console.log(`Доставка в ${country} будет стоить ${cost} кредитов`)
     break
   case 'чили':
     cost = 250
-    console.log(`Доставка в ${country} будет стоить ${cost} кредитов`)
     break
   case 'австралия':
     cost = 170
-    console.log(`Доставка в ${country} будет стоить ${cost} кредитов`)
     break
   case 'индия':
     cost = 80
-    console.log(`Доставка в ${country} будет стоить ${cost} кредитов`)
     break
   case 'ямайка':
     cost = 120
-    console.log(`Доставка в ${country} будет стоить ${cost} кредитов`)
     break
   default:
-    alert('В вашей стране доставка не доступна')
+    break
+}
+if(cost) {
+ console.log(`Доставка в ${country} будет стоить ${cost} кредитов`) 
+} else { 
+  alert('В вашей стране доставка не доступна')
 }
